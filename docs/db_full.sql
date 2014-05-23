2a41b92d828e616e91915aa74df2fca6f5944c2f
rphgds <rph.gds@gmail.com>
2014-05-22 20:09:22 -0700

    Ajuste das foreign keys na criação das tabelas

--- docs/db_full.sql
+++ docs/db_full.sql
@@ -22,19 +22,22 @@ CREATE TABLE informativo(
 cod_informativo INT PRIMARY KEY AUTO_INCREMENT,
 nom_informativo VARCHAR(50) NOT NULL,
 dsc_informativo LONGTEXT,
-cod_categoria INT FOREIGN KEY REFERENCES categoria (cod_categoria));
+cod_categoria INT,
+FOREIGN KEY (cod_categoria) REFERENCES categoria (cod_categoria));
 
 CREATE TABLE tipo_documento(
 cod_tipo_documento INT PRIMARY KEY AUTO_INCREMENT,
 nom_tipo_documento VARCHAR(50) NOT NULL,
 dsc_tipo_documento TEXT,
-cod_categoria INT FOREIGN KEY REFERENCES categoria (cod_categoria));
+cod_categoria INT, 
+FOREIGN KEY (cod_categoria) REFERENCES categoria (cod_categoria));
 
 CREATE TABLE documento(
 cod_documento INT PRIMARY KEY AUTO_INCREMENT,
 nom_documento VARCHAR(50) NOT NULL,
 bin_arquivo LONGBLOB NOT NULL,
-cod_tipo_documento INT FOREIGN KEY REFERENCES tipo_documento (cod_tipo_documento));
+cod_tipo_documento INT, 
+FOREIGN KEY (cod_tipo_documento) REFERENCES tipo_documento (cod_tipo_documento));
 
 CREATE TABLE cliente(
 cod_cliente INT PRIMARY KEY AUTO_INCREMENT,
@@ -43,14 +46,19 @@ nom_cliente VARCHAR(50) NOT NULL);
 CREATE TABLE usuario(
 cod_usuario INT PRIMARY KEY AUTO_INCREMENT,
 nom_usuario VARCHAR(50) NOT NULL,
-cod_cliente INT FOREIGN KEY REFERENCES cliente (cod_cliente),
+cod_cliente INT, 
+FOREIGN KEY (cod_cliente) REFERENCES cliente (cod_cliente),
 txt_login VARCHAR(50) NOT NULL UNIQUE,
 txt_senha VARCHAR(100) NOT NULL);
 
 CREATE TABLE cliente_categoria(
-cod_cliente INT FOREIGN KEY REFERENCES cliente (cod_cliente),
-cod_categoria INT FOREIGN KEY REFERENCES categoria (cod_categoria));
+cod_cliente INT, 
+FOREIGN KEY (cod_cliente) REFERENCES cliente (cod_cliente),
+cod_categoria INT, 
+FOREIGN KEY (cod_categoria) REFERENCES categoria (cod_categoria));
 
 CREATE TABLE usuario_tipo_documento(
-cod_tipo_documento INT FOREIGN KEY REFERENCES tipo_documento (cod_tipo_documento));
-cod_usuario INT FOREIGN KEY REFERENCES usuario (cod_usuario));
+cod_tipo_documento INT, 
+FOREIGN KEY (cod_tipo_documento) REFERENCES tipo_documento (cod_tipo_documento),
+cod_usuario INT, 
+FOREIGN KEY (cod_usuario) REFERENCES usuario (cod_usuario));

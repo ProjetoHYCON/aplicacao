9d57d60972e4d613d7f832b577d3977a90fb4146
alexandre3gomes <alexandre3gomes@gmail.com>
2014-05-22 23:57:27 -0300

    Script inicial.

---
+++ docs/db_full.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE hycon_db;
+
+USE hycon_db;
+
+CREATE TABLE link(
+cod_link INT PRIMARY KEY AUTO_INCREMENT,
+nom_link VARCHAR(50) NOT NULL,
+url_link TEXT NOT NULL);
+
+CREATE TABLE parceiro(
+cod_parceiro INT PRIMARY KEY AUTO_INCREMENT,
+nom_parceiro VARCHAR(50) NOT NULL,
+url_parceiro TEXT,
+bin_logo BLOB);
+
+CREATE TABLE categoria(
+cod_categoria INT PRIMARY KEY AUTO_INCREMENT,
+nom_categoria VARCHAR(50) NOT NULL,
+dsc_categoria TEXT);
+
+CREATE TABLE informativo(
+cod_informativo INT PRIMARY KEY AUTO_INCREMENT,
+nom_informativo VARCHAR(50) NOT NULL,
+dsc_informativo LONGTEXT,
+cod_categoria INT FOREIGN KEY REFERENCES categoria (cod_categoria));
+
+CREATE TABLE tipo_documento(
+cod_tipo_documento INT PRIMARY KEY AUTO_INCREMENT,
+nom_tipo_documento VARCHAR(50) NOT NULL,
+dsc_tipo_documento TEXT,
+cod_categoria INT FOREIGN KEY REFERENCES categoria (cod_categoria));
+
+CREATE TABLE documento(
+cod_documento INT PRIMARY KEY AUTO_INCREMENT,
+nom_documento VARCHAR(50) NOT NULL,
+bin_arquivo LONGBLOB NOT NULL,
+cod_tipo_documento INT FOREIGN KEY REFERENCES tipo_documento (cod_tipo_documento));
+
+CREATE TABLE cliente(
+cod_cliente INT PRIMARY KEY AUTO_INCREMENT,
+nom_cliente VARCHAR(50) NOT NULL);
+
+CREATE TABLE usuario(
+cod_usuario INT PRIMARY KEY AUTO_INCREMENT,
+nom_usuario VARCHAR(50) NOT NULL,
+cod_cliente INT FOREIGN KEY REFERENCES cliente (cod_cliente),
+txt_login VARCHAR(50) NOT NULL UNIQUE,
+txt_senha VARCHAR(100) NOT NULL);
+
+CREATE TABLE cliente_categoria(
+cod_cliente INT FOREIGN KEY REFERENCES cliente (cod_cliente),
+cod_categoria INT FOREIGN KEY REFERENCES categoria (cod_categoria));
+
+CREATE TABLE usuario_tipo_documento(
+cod_tipo_documento INT FOREIGN KEY REFERENCES tipo_documento (cod_tipo_documento));
+cod_usuario INT FOREIGN KEY REFERENCES usuario (cod_usuario));
